972617945dd824af1ea931638babfa32a725dcc9
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-10 15:31:00 +0530

    Add files via upload

---
+++ SwapingTwoNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class SwapingTwoNumbers
+    {
+        public static void SwapTheNumbers()
+        {
+            Console.WriteLine("enter two numbers");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("before swapping:{0} {1}", num1, num2);
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine("after swapping:{0} {1}", num1, num2);
+        }
+    }
+}
