87182168a386cd7508d142e0845f383d595935bc
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-10 15:28:30 +0530

    Add files via upload

---
+++ QuatientAndRemainder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class QuatientAndRemainder
+    {
+        public static void FindQuatientAndRemainder()
+        {
+            Console.WriteLine("enter the value of dividend and divisor");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            double quotient = dividend / divisor;
+            double reminder = dividend % divisor;
+            Console.WriteLine("Quotient=" + quotient + "\n Reminder=" + reminder);
+        }
+    }
+}
