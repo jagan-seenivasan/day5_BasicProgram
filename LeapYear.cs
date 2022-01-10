3b5adc08c265cbbe8ad86342e82a73d483aa9923
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-10 15:24:17 +0530

    Add files via upload

---
+++ LeapYear.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class LeapYear
+    {
+        public static void checkLeapYear()
+        {
+            int chk_Leapyear;
+            Console.WriteLine("Check whether a given year is leap year or not:");
+            Console.WriteLine("Input an year : ");
+            chk_Leapyear = Convert.ToInt32(Console.ReadLine());
+
+            if ((chk_Leapyear % 400) == 0)
+                Console.WriteLine("{0} is a leap year.", chk_Leapyear);
+            else if ((chk_Leapyear % 100) == 0)
+                Console.WriteLine("{0} is not a leap year.", chk_Leapyear);
+            else if ((chk_Leapyear % 4) == 0)
+                Console.WriteLine("{0} is a leap year.", chk_Leapyear);
+            else
+                Console.WriteLine("{0} is not a leap year.", chk_Leapyear);
+        }
+    }
+}
