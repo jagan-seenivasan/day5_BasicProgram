457a144ccae292ede2b542f58f53220dbf1696a1
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-10 15:25:11 +0530

    Add files via upload

---
+++ PrimeFactor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class PrimeFactor
+{
+	public static void ThePrime()
+	{
+		int n, i, p, k;
+
+		Console.Write("Enter a number:");
+		n = Convert.ToInt32(Console.ReadLine());
+
+		for (i = 2; i <= n; i++)
+		{
+			k = 2;
+			p = 1;
+			while (k < i)
+			{
+				if (i % k == 0)
+				{
+					p = 0;
+					break;
+				}
+				k++;
+			}
+			if (p == 1)
+			{
+				Console.WriteLine("Number is prime:" + i);
+			}
+		}
+
+	}
+}
